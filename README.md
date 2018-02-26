d772e36bdde11227f6346b198aa191b5a797656b
Kevin Mader <kevin.mader@4quant.com>
2018-02-26 20:50:01 +0100

    first version of repository with code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # conduit_analysis
+
+[![Binder with JupyterLab](https://img.shields.io/badge/launch-jupyterlab-red.svg)](http://mybinder.org/v2/gh/kmader/conduit_analysis/master?urlpath=lab)
+
+## notes
+
+Here a selection of 12 projections per frame (4 frames in total) per experiment (MI5 = conduit, and MI7 = no conduit).
+
+## notebooks
+
+- [![Overview](https://img.shields.io/badge/launch-overview-green.svg)](http://mybinder.org/v2/gh/kmader/conduit_analysis/master?filepath=notebooks/Overview.ipynb)
---
+++ circleci/execute.sh
@@ -0,0 +1,22 @@
+export PATH="$HOME/miniconda3/bin:$PATH"
+source activate vulc_analysis
+export QBI_HOME=$(pwd)
+
+python --version
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+
+# Generating documentation
+for nb in */*ipynb; do
+    jupyter nbconvert --ExecutePreprocessor.timeout=3600 --execute "$nb" --to markdown |& tee nb_to_md.txt
+    traceback=$(grep "Traceback (most recent call last):" nb_to_md.txt)
+    if [[ $traceback ]]; then
+        exit 1
+    fi
+done
+
+cd ~
+mkdir -p ${HOME}/doc
+mkdir -p ${HOME}/doc/notebooks
+cp -r ${QBI_HOME}/Lectures/* ${HOME}/doc/notebooks
+cp -r ./doc ${CIRCLE_ARTIFACTS}
